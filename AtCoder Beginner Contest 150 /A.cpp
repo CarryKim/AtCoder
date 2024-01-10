18df8c2533c03f417b6731fbe0e1b3ee077c9867
CarryKim <137850094+CarryKim@users.noreply.github.com>
2024-01-10 19:52:47 +0900

    A.cpp

--- AtCoder Beginner Contest 150 /A.cpp
+++ AtCoder Beginner Contest 150 /A.cpp
@@ -1,1 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int K,X;
+    cin >> K >> X;
+    if(K*500 >= X)
+        cout << "Yes";
+    else
+        cout << "No";
+}
